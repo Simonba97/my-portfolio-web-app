07d802f405ca15d6ca55f8bee86614def87ad161
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-05-13 16:29:51 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+        Este es un elemento con clases de Tailwind CSS.
+      </div>
+    </>
+  )
+}
+
+export default App
