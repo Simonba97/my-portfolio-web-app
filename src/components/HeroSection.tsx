ca750b2888fe8158ac468f4d774b50c220ab85c1
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-05-14 20:20:39 -0500

    HeroSection: Improve the display of main information

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,15 +1,19 @@
 import CodeFigure from "./common/CodeFigure"
-
+import logoSimon from "../assets/logoSimon.png";
 const HeroSection = () => {
     return (
         <div className="h-screen flex flex-col items-start justify-center">
-            <div className="pl-3 border-l-[1px] border-l-gray-300 space-y-5">
+            <div className="pl-3 border-l-[1px] border-l-gray-400 space-y-5">
                 <CodeFigure />
                 {/* Main information */}
-                <div id="mainInformation" className="flex flex-col items-start space-y-[-10px]">
-                    <h1 id="myName" className="text-2xl sm:text-3xl font-bold text-gray-200">Simón Bustamante Alzate</h1>
-                    <span id="myJob" className="text-xl font-extralight tracking-widest text-gray-300">Frontend Developer</span>
+                <div className="flex space-x-3">
+                    <img src={logoSimon} alt="memojiSimon" className="w-28" />
+                    <div id="mainInformation" className="flex flex-col items-start justify-center space-y-[-5px]">
+                        <h1 id="myName" className="text-4xl font-bold text-gray-200">I'm Simón</h1>
+                        <span id="myJob" className="text-xl font-extralight tracking-widest text-gray-300">Frontend Developer</span>
+                    </div>
                 </div>
+
                 <CodeFigure />
             </div>
 
