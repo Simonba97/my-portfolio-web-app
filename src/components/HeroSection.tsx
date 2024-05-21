918c291608bc78235346a1511ae5e43d05ed433e
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-05-20 20:10:52 -0500

    Hero Section: Integration framer-motion and animations in section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,26 @@
 import CodeFigure from "./common/CodeFigure"
 import logoSimon from "../assets/logoSimon.png";
+import { motion } from 'framer-motion';
+
 const HeroSection = () => {
     return (
         <section id="hero" className="h-screen flex flex-col items-center sm:items-start justify-center px-8">
-            <div className="pl-3 border-l-[1px] border-l-gray-400 space-y-5">
+            <div
+                className="pl-3 border-l-[1px] border-l-gray-400 space-y-5">
                 <CodeFigure />
                 {/* Main information */}
-                <div className="flex space-x-3">
+                <motion.div
+                    initial={{ opacity: 0, y: 0 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 2 }}
+                    className="flex space-x-3">
                     <img src={logoSimon} alt="memojiSimon" className="w-28" />
                     <div id="mainInformation" className="flex flex-col items-start justify-center space-y-[-5px]">
                         <h1 id="myName" className="text-4xl font-bold text-gray-200">Soy Simón</h1>
                         <span id="myJob" className="text-base sm:text-lg tracking-widest text-gray-400 font-mono">Frontend Developer</span>
                     </div>
-                </div>
+                </motion.div>
                 <CodeFigure />
             </div>
         </section>
--- src/components/common/CodeFigure.tsx
+++ src/components/common/CodeFigure.tsx
@@ -1,27 +1,38 @@
+import { motion } from 'framer-motion';
+
 const CodeFigure = () => {
     const pointFigure = <div className="w-2.5 h-2.5 bg-gray-300 rounded-full"></div>;
-    return <div className="flex flex-col space-y-2">
-        <div className="flex space-x-2">
-            <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
-            <div className="w-20 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
-            {pointFigure}
-        </div>
-        <div className="flex space-x-2">
-            <div className="w-56 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
-        </div>
-        <div className="flex space-x-2">
-            <div className="w-11 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
-            <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
-            {pointFigure}
-            <div className="w-16 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
-        </div>
-        <div className="flex space-x-2">
-            <div className="w-14 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
-            <div className="w-6 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
-            <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
-            <div className="w-16 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
-        </div>
-    </div>
+
+    const configBaseX = { min: 10, max: 200 };
+    const configBaseDuration = { min: 1, max: 1.5 };
+
+    return (
+        <div className="flex flex-col space-y-2">
+            <motion.div initial={{ opacity: 0, x: Math.floor(Math.random() * (configBaseX.max - configBaseX.min + 1)) + configBaseX.min }} animate={{ opacity: 1, x: 0, transition: { duration: Math.floor(Math.random() * (configBaseDuration.max - configBaseDuration.min + 1)) + configBaseDuration.min } }} className="flex space-x-2">
+                <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
+                <div className="w-20 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
+                {pointFigure}
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0, x: Math.floor(Math.random() * (configBaseX.max - configBaseX.min + 1)) + configBaseX.min }} animate={{ opacity: 1, x: 0, transition: { duration: Math.floor(Math.random() * (configBaseDuration.max - configBaseDuration.min + 1)) + configBaseDuration.min } }} className="flex space-x-2">
+                <div className="w-56 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0, x: Math.floor(Math.random() * (configBaseX.max - configBaseX.min + 1)) + configBaseX.min }} animate={{ opacity: 1, x: 0, transition: { duration: Math.floor(Math.random() * (configBaseDuration.max - configBaseDuration.min + 1)) + configBaseDuration.min } }} className="flex space-x-2">
+                <div className="w-11 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
+                <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
+                {pointFigure}
+                <div className="w-16 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0, x: Math.floor(Math.random() * (configBaseX.max - configBaseX.min + 1)) + configBaseX.min }} animate={{ opacity: 1, x: 0, transition: { duration: Math.floor(Math.random() * (configBaseDuration.max - configBaseDuration.min + 1)) + configBaseDuration.min } }} className="flex space-x-2">
+                <div className="w-14 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
+                <div className="w-6 h-2.5 bg-gradient-to-r from-amber-500 to-pink-500"></div>
+                <div className="w-28 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
+                <div className="w-16 h-2.5 bg-gradient-to-r from-indigo-400 to-cyan-400"></div>
+            </motion.div>
+        </div >
+    )
 }
 
 export default CodeFigure
